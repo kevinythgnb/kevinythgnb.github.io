fc7c8b814fc8f79a25f1f7f33fdb1ca973024b05
kevinythgnb <niveklc2001@gmail.com>
2026-01-05 07:28:01 -0600

    Gallery maintenance
    Add new photos and keyboard controls for modal navigation

--- photo.js
+++ photo.js
@@ -20,8 +20,8 @@ $(modal).append("<div id='nextzone'><div id='nextbkgd'></div><p>&rarr;</p></div>
 let currentImgIndex;
 document.body.appendChild(modal);
 
-imgBox.forEach((box, i) => {
-    box.addEventListener("click", () => {
+imgBox.forEach((box, i) => {    
+    let openModal = () => {
         $("body").css("overflow", "hidden");
         const smallImgSrc = box.querySelector("img").getAttribute("src");
         const imgSrc = smallImgSrc.split("/small")[0] + smallImgSrc.split("/small")[1]
@@ -33,27 +33,34 @@ imgBox.forEach((box, i) => {
         setTimeout(() => {
             modal.classList.add("shown");
         }, 1);
-    });
-    document.getElementById("closezone").addEventListener("click", () => {
+    }
+    box.addEventListener("click", openModal);
+
+    let closeModal = () => {
         modal.classList.remove("shown");
         setTimeout(() => {
             modal.classList.remove("onscreen");
         }, 300);
         $("body").css("overflow", "auto");
-    });
-});
+    }
+    document.getElementById("closezone").addEventListener("click", closeModal);
+    document.addEventListener("keydown", (k) => {
+        if (k.key === "Escape") {
+            closeModal();
+        }
+    })
+})
 
 const allImgs = document.querySelectorAll("#gallery img");
 
-const prevBtn = document.getElementById("prevzone");
 let prevImgSrc = () => {
     currentImgIndex--;
     if (currentImgIndex < 0) {
         currentImgIndex = allImgs.length - 1
     }
     return allImgs[currentImgIndex].src.split("/small")[0] + allImgs[currentImgIndex].src.split("/small")[1];
-};
-prevBtn.onclick = () => {
+}
+let goPrev = () => {
     $(modalImg).toggleClass("shown hidden");
     $(modalCap).toggleClass("shown hidden");
     setTimeout(() => {
@@ -64,17 +71,22 @@ prevBtn.onclick = () => {
         $(modalImg).toggleClass("shown hidden");
         $(modalCap).toggleClass("shown hidden");
     }, 300);
-};
+}
+document.getElementById("prevzone").addEventListener("click", goPrev);
+document.addEventListener("keydown", (k) => {
+    if (k.key === "ArrowLeft") {
+        goPrev();
+    }
+})
 
-const nextBtn = document.getElementById("nextzone");
 let nextImgSrc = () => {
     currentImgIndex++;
     if (currentImgIndex >= allImgs.length) {
         currentImgIndex = 0
     }
     return allImgs[currentImgIndex].src.split("/small")[0] + allImgs[currentImgIndex].src.split("/small")[1];
-};
-nextBtn.onclick = () => {
+}
+let goNext = () => {
     $(modalImg).toggleClass("shown hidden");
     $(modalCap).toggleClass("shown hidden");
     setTimeout(() => {
@@ -85,5 +97,10 @@ nextBtn.onclick = () => {
         $(modalImg).toggleClass("shown hidden");
         $(modalCap).toggleClass("shown hidden");
     }, 300);
-
-};
+}
+document.getElementById("nextzone").addEventListener("click", goNext);
+document.addEventListener("keydown", (k) => {
+    if (k.key === "ArrowRight") {
+        goNext();
+    }
+})
